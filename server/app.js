c6143d831942b01ba8451a81eb7d002d0d9b2d4b
CodeFiroz <khanfiroz4045@gmail.com>
2025-05-02 22:10:17 +0530

    backend setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-console.log("Hii backend");
+import express from "express"
+import config from "dotenv"
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res)=>{
+    res.send("Hii Server");
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server is start :: http://localhost:"+PORT);
+})
